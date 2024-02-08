fbeeb57e20a50d515d8f664638a7e8d2875eee0c
franmontaldo <franmontaldo@gmail.com>
2024-02-08 20:38:07 -0300

    added tests

--- ArtGalleryExample/ArtGalleryExampleTests/ArtGalleryExampleTests.swift
+++ ArtGalleryExample/ArtGalleryExampleTests/ArtGalleryExampleTests.swift
@@ -6,31 +6,81 @@
 //
 
 import XCTest
+import Combine
 @testable import ArtGalleryExample
+@testable import DataLayer
+@testable import PresentationLayer
+import SwiftUI
 
-final class ArtGalleryExampleTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+class ArtGalleryViewModelTests: XCTestCase {
+    
+    var viewModel: ArtGalleryViewModel!
+    var service: MockArtAPIService!
+    var cancellables: Set<AnyCancellable> = []
+    
+    override func setUp() {
+        super.setUp()
+        service = MockArtAPIService()
+        viewModel = ArtGalleryViewModel(service: service)
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    override func tearDown() {
+        viewModel = nil
+        service = nil
+        super.tearDown()
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    
+    func testFetchArtworksSuccess() {
+        // Arrange
+        let expectation = XCTestExpectation(description: "Fetch artworks success")
+        let artwork = DataLayer.Artwork(id: 1, title: "Artwork 1", artistDisplay: "Artist 1", dateDisplay: "2022", mediumDisplay: "Oil on canvas", description: "Description 1", imageID: "1")
+        let pagination = DataLayer.Pagination(total: 1, limit: 20, offset: 0, totalPages: 1, currentPage: 1, nextURL: "")
+        let response: (artworks: [DataLayer.Artwork], pagination: DataLayer.Pagination) = ([artwork], pagination)
+        service.mockArtworksResponse = response
+        var receivedArtworks: [ArtworkViewModel] = []
+        
+        // Act
+        viewModel.fetchArtworks()
+        viewModel.$artworks
+            .sink { artworks in
+                guard !artworks.isEmpty else { return }
+                receivedArtworks = artworks
+                expectation.fulfill()
+            }
+            .store(in: &cancellables)
+        
+        // Assert
+        wait(for: [expectation], timeout: 10)
+        XCTAssertEqual(receivedArtworks.count, 1)
+        XCTAssertEqual(receivedArtworks.first?.id, artwork.id)
+        XCTAssertEqual(receivedArtworks.first?.title, artwork.title)
+        XCTAssertEqual(receivedArtworks.first?.artistDisplay, artwork.artistDisplay)
+        XCTAssertEqual(receivedArtworks.first?.description, artwork.description)
+        XCTAssertEqual(receivedArtworks.first?.imageID, artwork.imageID)
     }
+}
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+class MockArtAPIService: ArtAPIServiceProtocol {
+    var mockArtworksResponse: (artworks: [DataLayer.Artwork], pagination: DataLayer.Pagination)?
+    var mockError: Error?
+    
+    func fetchArtworks(page: Int, limit: Int) -> AnyPublisher<(artworks: [DataLayer.Artwork], pagination: DataLayer.Pagination), Error> {
+        if let mockArtworksResponse = mockArtworksResponse {
+            return Just(mockArtworksResponse)
+                .setFailureType(to: Error.self)
+                .eraseToAnyPublisher()
+        } else if let mockError = mockError {
+            return Fail(error: mockError)
+                .eraseToAnyPublisher()
+        } else {
+            fatalError("Mock data or error not set for fetchArtworks")
         }
     }
-
+    
+    func fetchArtworkImage(withID imageID: String) -> AnyPublisher<Image, Error> {
+        let dummyImage = Image(systemName: "photo")
+        return Just(dummyImage)
+            .setFailureType(to: Error.self)
+            .eraseToAnyPublisher()
+    }
 }
